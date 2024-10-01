4b20a302e9c96b0cb6caa1274b4842c277fd50c2
Erdem Kaya <kerdem@live.com>
2024-10-01 13:28:40 +0200

    do while

---
+++ Bolum2/3-doWhile/main.js
@@ -0,0 +1,7 @@
+let i = 3;
+/* doWhile , while dan farkli olarak do yazdiktan sonra komut dogru mu degil mi bakmadan ilk seferde do icindekiler calistirlir. daha sonra while kondisyonuna bakar  */
+
+do {
+  console.log(i);
+  i++;
+}while(i < 5);
