9be43667b80572eed9f3e0460ad0985a17c336fe
Erdem Kaya <kerdem@live.com>
2024-10-01 13:18:28 +0200

    While

---
+++ Bolum2/2-while/main.js
@@ -0,0 +1,25 @@
+const adSoyad=['erdem','kaya','aylin'];
+let i=0;
+
+while(i < 10)
+{
+  console.log(i);
+  i++;
+}
+
+i=0;
+
+while(i < adSoyad.length)
+{
+  console.log(adSoyad[i]);
+  i++;
+}
+/* i degerini artirmazsak dongu sonsuza kadar devam eder */
+
+
+i=6;
+while(i > 3)
+{
+  console.log(i);
+  i--;
+}
