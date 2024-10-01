b181e460a13ad5625773ed3e63a4e0cbb30dc485
Erdem Kaya <kerdem@live.com>
2024-10-01 12:40:16 +0200

    karsilastirma

---
+++ Bolum1/11-karsilastirma/main.js
@@ -0,0 +1,22 @@
+/* let yas=29;
+
+console.log(yas == 29);
+console.log(yas == '29'); */
+
+/* tipine gore yani number mi string mi anlamasi icin 3 esttir kullan. 2 essittire js kendi degiskligini uygular ve true doner */
+/* console.log(yas === 29);
+console.log(yas === '29'); */
+
+
+let yas='29';
+
+console.log(yas+1);
+console.log(typeof yas);
+
+/* Ifadeyi stringe cevirmek istersen String , sayiya cevirmek icin Number */
+yas = Number(yas);
+console.log(yas+1);
+console.log(typeof yas);
+
+let sonuc=Number('Erdem')
+console.log(sonuc)
