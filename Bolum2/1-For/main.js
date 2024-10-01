9494a5af6abf24d8cecb6f9451aab998b1921f58
Erdem Kaya <kerdem@live.com>
2024-10-01 13:06:03 +0200

    For kullanimi

---
+++ Bolum2/1-For/main.js
@@ -0,0 +1,15 @@
+
+for (let i=0;i<10;i++)
+{
+  console.log(i);
+}
+
+console.log("Döngu bitti");
+
+const adSoyad=['erdem','kaya','aylin'];
+for(let i=0;i < adSoyad.length;i++)
+{
+  /* console.log(adSoyad[i]); */
+  let html=`<div>${adSoyad[i]}</div>`;
+  console.log(html);
+}
