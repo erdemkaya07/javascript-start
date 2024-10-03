fe53a2aabae245a1eafc5df4b597f87a1e87b867
Erdem Kaya <kerdem@live.com>
2024-10-03 08:39:25 +0200

    FormValidation

--- Bolum6/4-KeyUp-Event/main.js
+++ Bolum6/4-KeyUp-Event/main.js
@@ -1,14 +1,23 @@
 const form=document.querySelector('.signupForm');
 const message=document.querySelector('.message');
+const usernamePattern=/^[a-z]{6,12}$/;
 
 form.addEventListener('submit', e=>{
   e.preventDefault(); //Form gonderirken Sayfanin yenilenmesini engeller.
   const username=form.username.value;
-  const usernamePattern=/^[a-z]{6,12}$/;
   if(usernamePattern.test(username)){
     message.textContent='Basarili';
   } else {
     message.textContent='Lutfen hepsini kucuk harf 6-12 karakter arasi giriniz.'
 
+  }
+})
+
+form.username.addEventListener('keyup', e => {
+  /* console.log(e.target.value) */
+  if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class','success');
+  } else {
+    form.username.setAttribute('class','error');
   }
 })
